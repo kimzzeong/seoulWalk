257ec6a39c534e010203b476bdbd2823a0367bb7
Wans <cklhbj11@nate.com>
2021-11-07 16:46:03 +0900

    Activity 추가123

--- app/src/main/java/com/example/seoulwalk/Activity/ActivityLogin.java
+++ app/src/main/java/com/example/seoulwalk/Activity/ActivityLogin.java
@@ -3,6 +3,7 @@ package com.example.seoulwalk.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.seoulwalk.R;
 
@@ -12,5 +13,7 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.e("여기는 ","로그인");
     }
 }
