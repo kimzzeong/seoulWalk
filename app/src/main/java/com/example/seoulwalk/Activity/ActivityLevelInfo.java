b9e822f6606ed294325efa8cab0110b5f9cb4260
kimzzeong <74105034+kimzzeong@users.noreply.github.com>
2021-11-08 16:37:23 +0900

    main activity layout finish

--- app/src/main/java/com/example/seoulwalk/Activity/ActivityLevelInfo.java
+++ app/src/main/java/com/example/seoulwalk/Activity/ActivityLevelInfo.java
@@ -1,6 +1,8 @@
 package com.example.seoulwalk.Activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +14,15 @@ public class ActivityLevelInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_info);
+
+        //커스텀 액션바 세팅
+        Toolbar toolbar;
+        ActionBar actionBar;
+        toolbar = findViewById(R.id.level_actionbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(false); //뒤로가기 아이콘 없앰
     }
 }
