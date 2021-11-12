9c7cd3cf9ef5758266d38369356e4eb7bb28c4d8
kimzzeong <74105034+kimzzeong@users.noreply.github.com>
2021-11-12 11:26:41 +0900

    커뮤니티 진행중

--- app/src/main/java/com/example/seoulwalk/Activity/ActivityGoal.java
+++ app/src/main/java/com/example/seoulwalk/Activity/ActivityGoal.java
@@ -25,7 +25,7 @@ public class ActivityGoal extends AppCompatActivity {
     Button goal_maintenance_btn, goal_step_up_btn;
     int user_level = 2; //현재 유저의 레벨
     ArrayList<String> items = new ArrayList<>();
-    TextView level_text;
+    TextView level_text,level_week_goal_step,level_day_goal_step;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +48,8 @@ public class ActivityGoal extends AppCompatActivity {
         goal_maintenance_btn = findViewById(R.id.goal_maintenance_btn);
         goal_step_up_btn = findViewById(R.id.goal_step_up_btn);
         level_text = findViewById(R.id.level_text);
+        level_week_goal_step = findViewById(R.id.level_week_goal_step);
+        level_day_goal_step = findViewById(R.id.level_day_goal_step);
 
         goal_maintenance_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,10 +97,16 @@ public class ActivityGoal extends AppCompatActivity {
               //  Log.e("Log",items.get(position));
                 if(position == 0){
                     level_text.setText("Lv.3 도전 한다면?");
+                    level_week_goal_step.setText("주간 목표 걸음 수 : 70,000 걸음");
+                    level_day_goal_step.setText("일간 권장 걸음 수 : 10,000 걸음");
                 }else if(position == 1){
                     level_text.setText("Lv.4 도전 한다면?");
+                    level_week_goal_step.setText("주간 목표 걸음 수 : 80,000 걸음");
+                    level_day_goal_step.setText("일간 권장 걸음 수 : 12,000 걸음");
                 }else{
                     level_text.setText("Lv.5 도전 한다면?");
+                    level_week_goal_step.setText("주간 목표 걸음 수 : 90,000 걸음");
+                    level_day_goal_step.setText("일간 권장 걸음 수 : 14,000 걸음");
                 }
             }
 
